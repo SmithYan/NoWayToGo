cc51b789097bde47b878987b40f56bbd56c68766
SmithYan <wy521wywy@163.com>
2018-08-01 18:47:03 +0800

    初步完成急需修正

--- src/mainForm.java
+++ src/mainForm.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -32,6 +34,117 @@ public class mainForm extends Frame {
 		System.out.print("游戏开始");
 		frame = new mainForm();
 		frame.launch();
+	}
+
+	public void actionss() {
+		MouseAdapter l = new MouseAdapter() {// 匿名内部类
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// 设定球的初始值为空
+				globe = null;
+				// 循环球集合
+				for (Globe g : globes) {
+					// 如果点击到了球
+					if (e.getX() >= g.point.x && e.getY() >= g.point.y && e.getY() <= g.point.x + g.radius
+							&& e.getY() <= g.point.y + g.radius) {
+						// 将点击的球赋值给本界面的球
+						globe = g;
+						// 将此营地的球移除
+						globe.home.Remove();
+						for (Home home : homes) {
+							if (home.globe == g) {
+								home.globe = null;
+							}
+						}
+						// 存储鼠标相对于球的坐标
+						mouseDownGlobeX = e.getX() - g.point.x;
+						mouseDownGlobeY = e.getY() - g.point.y;
+						break;
+					}
+				}
+				// 存储鼠标相对于界面的坐标
+				mouseDownX = e.getX();
+				mouseDownY = e.getY();
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// 当球不为空的时候
+				if (globe != null) {
+					// 将营地赋值为空
+					Home cs = null;
+					// 定义Boolean变量isRelation
+					Boolean isRelation = false;
+					// 循环所有营地
+					for (Home home : homes) {
+						// 当鼠标到达营地的时候
+						if (e.getX() >= home.point.x - 20 && e.getY() >= home.point.y - 20
+								&& e.getX() <= home.point.x + home.radius + 20
+								&& e.getY() <= home.point.y + home.radius + 20) {
+							// 将营地赋值给当前界面的营地
+							cs = home;
+							break;
+						}
+					}
+					// 当营地不为空的时候
+					if (cs != null) {
+						// 循环与之相连的营地
+						for (Home cam : globe.home.another) {
+							// 当鼠标所在营地是其相连营地的时候
+							if (cam == cs) {
+								// IDRelation赋值为true（两个营地有关系）
+								isRelation = true;
+							}
+						}
+					}
+					// 当两个营地没有关系时，此界面的营地赋值为空
+					if (!isRelation) {
+						cs = null;
+					}
+					// 当营地和球都不为空的时候
+					if (cs != null && cs.globe == null) {
+						// 将球赋值为此营地的球
+						globe.point = cs.point;
+						// 将球的营地更改为鼠标当前的营地
+						globe.home = cs;
+					} else {
+						// 球归位
+						globe.point = new Point(e.getX() - mouseDownGlobeX, e.getY() - mouseDownGlobeY);
+
+					}
+					// 球的坐标为所在营地的坐标
+					globe.point = globe.home.point;
+					// 营地的球是当前球
+					globe.home.globe = globe;
+				}
+				globe = null;
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				if (globe != null) {
+					globe.point = new Point(e.getX() - 25, e.getY() - 25);
+				}
+			}
+		};
+		frame.addMouseListener(l);// 处理鼠标的操作事件
+		frame.addMouseMotionListener(l);// 处理鼠标的滑动事件
 	}
 
 	public void newMainForm() {
@@ -94,6 +207,7 @@ public class mainForm extends Frame {
 		globes.add(globe2);
 		globes.add(globe3);
 		globes.add(globe4);
+
 	}
 
 	public void launch() {
@@ -120,6 +234,7 @@ public class mainForm extends Frame {
 		this.setVisible(true);
 		newMainForm();
 		paint(this.getGraphics());
+		actionss();
 	}
 
 	@Override
